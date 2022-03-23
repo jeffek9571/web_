6a5cac3ee0a78d8bff1c91304dbc5c7d55851b79
BB <jeffek9571@hotmail.com>
2022-03-23 14:48:49 +0800

    loader

---
+++ src/components/nav_item.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref} from 'vue'
+    defineProps({
+      msg: String
+    })
+</script>
+
+<template>
+    <ul class="ul-list" @click="clicka">
+        <ul>{{ msg }}</ul>
+        <img src="../assets/arrow.png" alt="" >
+    </ul>
+</template>
+
+<style scoped>
+img{
+  width: 10px;
+  height: 10px;
+}
+.ul-list:hover {
+  background-color: grey;
+}
+.ul-list{
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+}
+ul{
+    border-radius: 10px;
+    width: 50%;
+    color: #d9621e;
+    margin: 8px;
+    padding: 5px;
+}
+</style>
